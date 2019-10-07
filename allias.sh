2aea8eefbd22979d46ec75d7ebb689e03c1f9a01
rostegg <rostik.nikolaenko@gmail.com>
2019-10-07 10:06:29 +0300

    👶

--- allias.sh
+++ allias.sh
@@ -1,6 +1,5 @@
 touch ~/.bash_aliases
 
 echo "alias cmall='rnd-commit-all && auto-push'" >> ~/.bash_aliases
-# change to func
-echo "pkgex() { ldconfig -p | grep \"$1\" }" >> ~/.bash_aliases
+echo "pkgex() { ldconfig -p | grep \"\$1\" ; }" >> ~/.bash_aliases
 . ~/.bash_aliases
