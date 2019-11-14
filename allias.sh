da92ce52a5d998c269d7843537a5f14358d902af
rostegg <rostik.nikolaenko@gmail.com>
2019-11-14 11:38:38 +0200

    👽

--- allias.sh
+++ allias.sh
@@ -1,5 +1,6 @@
 touch ~/.bash_aliases
 
 echo "alias cmall='rnd-commit-all && auto-push'" >> ~/.bash_aliases
+echo "alias ext-debug='journalctl /usr/bin/gnome-shell -f'" >> ~/.bash_aliases
 echo "pkgex() { ldconfig -p | grep \"\$1\" ; }" >> ~/.bash_aliases
 . ~/.bash_aliases
