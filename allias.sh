b100964b072e0c6ffe0e41abe7b2cdf204e065a8
rostegg <rostik.nikolaenko@gmail.com>
2020-05-26 22:08:40 +0300

    ➕

--- allias.sh
+++ allias.sh
@@ -1,5 +1,6 @@
+rm ~/.bash_aliases
 touch ~/.bash_aliases
-
+echo "alias rewifi='nmcli radio wifi off && nmcli radio wifi on'" >> ~/.bash_aliases 
 echo "alias cmall='rnd-commit-all && auto-push'" >> ~/.bash_aliases
 echo "alias ext-debug='journalctl /usr/bin/gnome-shell -f'" >> ~/.bash_aliases
 echo "pkgex() { ldconfig -p | grep \"\$1\" ; }" >> ~/.bash_aliases
