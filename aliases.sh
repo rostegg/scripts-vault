17b95ee059a3cef4f78bdba3de7cb7bf51d0fa04
rostegg <rostik.nikolaenko@gmail.com>
2020-05-28 00:00:50 +0300

    🎹

--- aliases.sh
+++ aliases.sh
@@ -2,14 +2,14 @@
 rm ~/.bash_aliases
 touch ~/.bash_aliases
 #<->
-# Restart WiFi service
+#   Restart WiFi service
 echo "alias rewifi='nmcli radio wifi off && nmcli radio wifi on'" >> ~/.bash_aliases 
-# Random commit and auto-push
+#   Random commit and auto-push
 echo "alias cmall='rnd-commit-all && auto-push'" >> ~/.bash_aliases
-# Debug info for gnome webextensions
+#   Debug info for gnome webextensions
 echo "alias ext-debug='journalctl /usr/bin/gnome-shell -f'" >> ~/.bash_aliases
-# Package info
-# First parameter - name of package
+#   Package info
+#   Usage: pkgex <package_name>
 echo "pkgex() { ldconfig -p | grep \"\$1\" ; }" >> ~/.bash_aliases
 #>-<
 . ~/.bash_aliases
