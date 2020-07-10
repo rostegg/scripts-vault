b7a63ad3323cae58ebed366da891b0cafbc5f818
rostegg <rostik.nikolaenko@gmail.com>
2020-07-10 21:00:13 +0300

    🐣

--- aliases.sh
+++ aliases.sh
@@ -3,7 +3,7 @@ rm ~/.bash_aliases
 touch ~/.bash_aliases
 #<->
 #   Restart WiFi service
-echo "alias rewifi='nmcli radio wifi off && nmcli radio wifi on'" >> ~/.bash_aliases 
+echo "alias rewifi='nmcli radio wifi off && nmcli radio wifi on'" >> ~/.bash_aliases
 #   Random commit and auto-push
 echo "alias cmall='rnd-commit-all && auto-push'" >> ~/.bash_aliases
 #   Debug info for gnome webextensions
@@ -11,5 +11,11 @@ echo "alias ext-debug='journalctl /usr/bin/gnome-shell -f'" >> ~/.bash_aliases
 #   Package info
 #   Usage: pkgex <package_name>
 echo "pkgex() { ldconfig -p | grep \"\$1\" ; }" >> ~/.bash_aliases
+#   cd ..
+echo "alias ..='cd ..'" >> ~/.bash_aliases
+#   cd ../..
+echo "alias ...='cd ../..'" >> ~/.bash_aliases
+#   cd ../../..
+echo "alias ....='cd ../../..'" >> ~/.bash_aliases
 #>-<
 . ~/.bash_aliases
--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 SCRIPT_FOLDER="${PWD}/scripts"
+
+for file in $SCRIPT_FOLDER/*; do sudo chmod +x $file; done
+
 printf "Moving scripts to \e[32m/usr/bin\e[0m folder...\n"
 
 sudo cp --verbose "${SCRIPT_FOLDER}"/* /usr/bin
 
+. aliases.sh
 . list.sh
